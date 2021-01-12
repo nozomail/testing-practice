1650717ff863a485a71443cea23e6b6ed6d354b0
nozomail <nozomi.mail@icloud.com>
2021-01-12 23:28:03 +1100

    Add unit test for Form

--- src/Form.tsx
+++ src/Form.tsx
@@ -12,27 +12,28 @@ function Form({ setdata, setfilters }: Props) {
 
   async function handleFormSubmit(e: React.FormEvent): Promise<void> {
     e.preventDefault();
-    const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`);
-    if (response.data.drinks) {
-      const modifiedData: DataProps[] = response.data.drinks.map((drink: OriginalDataProps) => {
-        const ingredients = [];
-        for (let i = 1; i < 16; i++) {
-          const ingredient = drink[`strIngredient${i + 1}`];
-          if (ingredient === null || ingredient === "") break;
-          ingredients.push(ingredient);
-        }
-
-        return {
-          idDrink: drink.idDrink,
-          strAlcoholic: drink.strAlcoholic,
-          strDrink: drink.strDrink,
-          strInstructions: drink.strInstructions,
-          strIngredients: ingredients,
-        };
-      });
-      setdata(modifiedData);
-      setfilters({ isAlcoholic: true, isNonAlcoholic: true });
-    }
+    const searchName = name.trim();
+    setname(searchName);
+    if (searchName === "") return setdata([]);
+    const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchName}`);
+    if (response.data.drinks === null) return setdata([]);
+    const modifiedData: DataProps[] = response.data.drinks.map((drink: OriginalDataProps) => {
+      const ingredients = [];
+      for (let i = 1; i < 16; i++) {
+        const ingredient = drink[`strIngredient${i + 1}`];
+        if (ingredient === null || ingredient === "") break;
+        ingredients.push(ingredient);
+      }
+      return {
+        idDrink: drink.idDrink,
+        strAlcoholic: drink.strAlcoholic,
+        strDrink: drink.strDrink,
+        strInstructions: drink.strInstructions,
+        strIngredients: ingredients,
+      };
+    });
+    setdata(modifiedData);
+    setfilters({ isAlcoholic: true, isNonAlcoholic: true });
   }
 
   return (
